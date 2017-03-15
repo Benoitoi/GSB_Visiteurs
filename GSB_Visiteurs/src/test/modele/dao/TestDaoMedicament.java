b70201b88b8261830904c4b14808e1577c288361
Benoit <Benoit@Benoitoi>
2017-03-15 15:50:09 +0100

    test dao medicament

---
+++ GSB_Visiteurs/src/test/modele/dao/TestDaoMedicament.java
@@ -0,0 +1,40 @@
+package test.modele.dao;
+
+import controleurs.CtrlVisiteur;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modele.dao.DaoMedicament;
+
+/**
+ *
+ * @author btssio
+ */
+public class TestDaoMedicament {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Test DaoMedicament\n");
+        try {
+            System.out.println("Test 1 : obtenir tous les medicaments");
+            System.out.println(DaoMedicament.getAllMedicaments());
+            System.out.println("Test 2 : un medicament par un depot legal innexistant\n");
+           if(DaoMedicament.getOneByDepotLegal("nonexistant") == null) {
+                System.out.println("succès");
+            } else {
+                System.out.println("échec");
+            }
+            System.out.println("Test 3 : un medicament par un depot legal existant\n");
+            if(DaoMedicament.getOneByDepotLegal("DEPRIL9") == null) {
+                System.out.println("échec");
+            } else {
+                System.out.println("succès");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CtrlVisiteur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
