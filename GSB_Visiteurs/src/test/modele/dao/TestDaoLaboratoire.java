f5d3720da92439e75aeb391473670e9eb8b6a215
Benoit <Benoit@Benoitoi>
2017-03-15 15:43:25 +0100

    test daolab

--- GSB_Visiteurs/src/test/modele/dao/TestDaoLaboratoire.java
+++ GSB_Visiteurs/src/test/modele/dao/TestDaoLaboratoire.java
@@ -17,10 +17,22 @@ public class TestDaoLaboratoire {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Test DaoVisiteur\n");
+        System.out.println("Test DaoLaboratoire\n");
         try {
             System.out.println("Test 1 : obtenir tous les laboratoires");
             System.out.println(DaoLaboratoire.getAllLaboratoires());
+            System.out.println("Test 2 : un laboratoire par un code innexistant\n");
+            if(DaoLaboratoire.getOneByCode("nonexistant") == null) {
+                System.out.println("succès");
+            } else {
+                System.out.println("échec");
+            }
+            System.out.println("Test 3 : un laboratoire par un code existant\n");
+            if(DaoLaboratoire.getOneByCode("BC") == null) {
+                System.out.println("échec");
+            } else {
+                System.out.println("succès");
+            }
         } catch (SQLException ex) {
             Logger.getLogger(CtrlVisiteur.class.getName()).log(Level.SEVERE, null, ex);
         }
